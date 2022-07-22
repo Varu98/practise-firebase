b3362c03ab437cc6e6a5f2dd59edd739949b2a14
Varu <vardaan.agarwal.98@gmail.com>
2022-07-22 23:52:41 +0530

    add : firebase changes ?

---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDz5IA5CXQjUNHeP0ydK1cEDX8CSo1QLhA",
+  authDomain: "practise-firebase-45cc9.firebaseapp.com",
+  projectId: "practise-firebase-45cc9",
+  storageBucket: "practise-firebase-45cc9.appspot.com",
+  messagingSenderId: "352349301006",
+  appId: "1:352349301006:web:e9013fac646120a1224f5f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
